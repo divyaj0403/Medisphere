527feb6d601924b0d7503f011414e2a0ce912f61
Divya <divyajunghare04@gmail.com>
2024-05-03 16:39:40 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,41 +1,26 @@
 import os
 import pickle
 import streamlit as st
-from streamlit_option_menu import option_menu
-
 
 # Set page configuration
 st.set_page_config(page_title="Health Assistant",
                    layout="wide",
                    page_icon="🧑‍⚕️")
 
-    
-# getting the working directory of the main.py
+# Sidebar for navigation
+selected = st.sidebar.selectbox('Multiple Disease Prediction System',
+                                 ['Diabetes Prediction',
+                                  'Heart Disease Prediction',
+                                  "Parkinson's Prediction"])
+
+# Load saved models
 working_dir = os.path.dirname(os.path.abspath(__file__))
-
-# loading the saved models
-
 diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
-
 heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
-
 parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
 
-# sidebar for navigation
-with st.sidebar:
-    selected = option_menu('Multiple Disease Prediction System',
-
-                           ['Diabetes Prediction',
-                            'Heart Disease Prediction',
-                            'Parkinsons Prediction'],
-                           menu_icon='hospital-fill',
-                           icons=['activity', 'heart', 'person'],
-                           default_index=0)
-
-
-# Diabetes Prediction Page
-if selected == 'Diabetes Prediction':
-
+# Function to predict diabetes
+def predict_diabetes():
     # page title
     st.title('Diabetes Prediction using ML')
 
@@ -66,14 +51,11 @@ if selected == 'Diabetes Prediction':
     with col2:
         Age = st.text_input('Age of the Person')
 
-
     # code for Prediction
     diab_diagnosis = ''
 
     # creating a button for Prediction
-
     if st.button('Diabetes Test Result'):
-
         user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
                       BMI, DiabetesPedigreeFunction, Age]
 
@@ -88,9 +70,8 @@ if selected == 'Diabetes Prediction':
 
     st.success(diab_diagnosis)
 
-# Heart Disease Prediction Page
-if selected == 'Heart Disease Prediction':
-
+# Function to predict heart disease
+def predict_heart_disease():
     # page title
     st.title('Heart Disease Prediction using ML')
 
@@ -114,35 +95,12 @@ if selected == 'Heart Disease Prediction':
     with col3:
         fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
 
-    with col1:
-        restecg = st.text_input('Resting Electrocardiographic results')
-
-    with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved')
-
-    with col3:
-        exang = st.text_input('Exercise Induced Angina')
-
-    with col1:
-        oldpeak = st.text_input('ST depression induced by exercise')
-
-    with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment')
-
-    with col3:
-        ca = st.text_input('Major vessels colored by flourosopy')
-
-    with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
-
     # code for Prediction
     heart_diagnosis = ''
 
     # creating a button for Prediction
-
     if st.button('Heart Disease Test Result'):
-
-        user_input = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+        user_input = [age, sex, cp, trestbps, chol, fbs]
 
         user_input = [float(x) for x in user_input]
 
@@ -155,9 +113,8 @@ if selected == 'Heart Disease Prediction':
 
     st.success(heart_diagnosis)
 
-# Parkinson's Prediction Page
-if selected == "Parkinsons Prediction":
-
+# Function to predict Parkinson's disease
+def predict_parkinsons():
     # page title
     st.title("Parkinson's Disease Prediction using ML")
 
@@ -172,72 +129,12 @@ if selected == "Parkinsons Prediction":
     with col3:
         flo = st.text_input('MDVP:Flo(Hz)')
 
-    with col4:
-        Jitter_percent = st.text_input('MDVP:Jitter(%)')
-
-    with col5:
-        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
-
-    with col1:
-        RAP = st.text_input('MDVP:RAP')
-
-    with col2:
-        PPQ = st.text_input('MDVP:PPQ')
-
-    with col3:
-        DDP = st.text_input('Jitter:DDP')
-
-    with col4:
-        Shimmer = st.text_input('MDVP:Shimmer')
-
-    with col5:
-        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
-
-    with col1:
-        APQ3 = st.text_input('Shimmer:APQ3')
-
-    with col2:
-        APQ5 = st.text_input('Shimmer:APQ5')
-
-    with col3:
-        APQ = st.text_input('MDVP:APQ')
-
-    with col4:
-        DDA = st.text_input('Shimmer:DDA')
-
-    with col5:
-        NHR = st.text_input('NHR')
-
-    with col1:
-        HNR = st.text_input('HNR')
-
-    with col2:
-        RPDE = st.text_input('RPDE')
-
-    with col3:
-        DFA = st.text_input('DFA')
-
-    with col4:
-        spread1 = st.text_input('spread1')
-
-    with col5:
-        spread2 = st.text_input('spread2')
-
-    with col1:
-        D2 = st.text_input('D2')
-
-    with col2:
-        PPE = st.text_input('PPE')
-
     # code for Prediction
     parkinsons_diagnosis = ''
 
-    # creating a button for Prediction    
+    # creating a button for Prediction
     if st.button("Parkinson's Test Result"):
-
-        user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
-                      RAP, PPQ, DDP,Shimmer, Shimmer_dB, APQ3, APQ5,
-                      APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
+        user_input = [fo, fhi, flo]
 
         user_input = [float(x) for x in user_input]
 
@@ -249,3 +146,11 @@ if selected == "Parkinsons Prediction":
             parkinsons_diagnosis = "The person does not have Parkinson's disease"
 
     st.success(parkinsons_diagnosis)
+
+# Main page content based on user selection
+if selected == 'Diabetes Prediction':
+    predict_diabetes()
+elif selected == 'Heart Disease Prediction':
+    predict_heart_disease()
+elif selected == "Parkinson's Prediction":
+    predict_parkinsons()
